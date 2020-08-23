5d888efab0ab5056639d712705e8ff6399e0f4a0
saibhargav85 <saibhargav85@gmail.com>
2020-08-23 22:25:36 +0530

    broadcast message

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io()
 socket.on('connect', function () {
     console.log('connected to server');
 
-    socket.emit('createMessage', {
-        from: 'sai',
-        text: 'Hi'
-    });
 });
 
 socket.on('disconnect', function () {
